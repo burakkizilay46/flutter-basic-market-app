26ad332474e54ef5b00728cac153d93f40033173
Burak Kızılay <burak.kizilay@westerops.com>
2024-01-07 01:30:46 +0300

    provider add main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:market/core/navigation/navigation_routes.dart';
+import 'package:market/feature/splash/provider/splash_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(create: (context) => SplashProvider('initalData'), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
