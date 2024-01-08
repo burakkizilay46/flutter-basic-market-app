9e228bcf93df4e8764170d6ff4dd9983abfbddeb
Burak Kızılay <burak.kizilay@westerops.com>
2024-01-08 23:04:14 +0300

    dio package added/ fetched data from internet

--- lib/feature/market/view/market_view.dart
+++ lib/feature/market/view/market_view.dart
@@ -1,12 +1,62 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class MarketView extends StatelessWidget {
+class MarketView extends StatefulWidget {
   const MarketView({super.key});
 
   @override
+  State<MarketView> createState() => _MarketViewState();
+}
+
+class _MarketViewState extends State<MarketView> {
+  late Future<List<dynamic>> products;
+
+  Future<List<dynamic>> getProducts() async {
+    final dio = Dio();
+    final response = await dio.get('https://fakestoreapi.com/products');
+    return response.data;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    products = getProducts();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Market View'),
+    return Scaffold(
+      body: FutureBuilder<List<dynamic>>(
+        future: products,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator()); // Show a loading indicator while data is being fetched
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Text('No data available'); // Handle the case where there is no data
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Container(height: 128, child: Image.network(snapshot.data![index]['image'])),
+                        Text(snapshot.data![index]['title'].toString())
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
+
+/* Text(snapshot.data![index]['title'].toString()); */
